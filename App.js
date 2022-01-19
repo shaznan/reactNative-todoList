a0165864101c39cb87f9ce7c01aaf83b2bbfdb9c
shaznan <shaznanfairozesally@gmail.com>
2022-01-19 22:57:39 +0530

    test

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import MainPageContainer from './container/MainPageContainer';
+import MainPageContainer from './src/container/MainPageContainer';
 
 export default function App() {
   return (
--- src/container/MainPageContainer.js
+++ src/container/MainPageContainer.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import {useState} from 'react'
-import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Image, Button } from 'react-native';
+
 
 export default function MainPageContainer() {
     const [textInput, setTextInput] = useState('')
@@ -16,6 +17,10 @@ export default function MainPageContainer() {
           <Text style={styles.mainText}>What's on your bucket list for today?</Text>
           <TextInput style={styles.textInput} placeholder="Write Something.."  onChangeText={setTextInput} value={textInput} />
           <TextInput style={styles.textInput} placeholder="imageUrl" onChangeText={setImageUrl} value={imgUrl} />
+          <View>
+            <Button>Click Me</Button>
+              <Image style={styles.addbutton} source={{uri: 'https://cdn-icons.flaticon.com/png/512/3416/premium/3416075.png?token=exp=1642524540~hmac=63c9843d886a36b8020ab252f89b744f'}} />
+          </View>
       </View>
     </SafeAreaView>
   );
@@ -46,5 +51,13 @@ title:{
     marginTop: 12,
     borderWidth: 0.2,
     padding: 12,
+  },
+  addbutton:{
+    width:20,
+    height:20,
+  },
+  addbutton:{
+    width: 100,
+    height: 100
   }
 });
