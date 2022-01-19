d7a8660b84d315e43b3f906fdc40d52d2bcf7ca8
shaznan <shaznanfairozesally@gmail.com>
2022-01-20 01:11:12 +0530

    add flatlist items, changed background color

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, {Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import MainPageContainer from './src/container/MainPageContainer';
--- src/container/MainPageContainer.js
+++ src/container/MainPageContainer.js
@@ -1,15 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import {useState} from 'react'
-import { StyleSheet, Text, View, SafeAreaView, TextInput, Image, Button } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Image, Button, FlatList, ScrollView } from 'react-native';
 
 
 export default function MainPageContainer() {
     const [textInput, setTextInput] = useState('')
     const [imgUrl, setImageUrl]=useState('')
 
+    const DATA = [
+      {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item',
+      },
+      {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Second Item',
+      },
+      {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+      },
+    ];
+
+    const renderItem = ({item})=>(
+      <View style={styles.listItem}>
+        <Text style={styles.listItemText}>{item.title}</Text>
+        <View style={styles.iconContainer}>
+        <Image style={styles.iconimg} source={require('../Assets/eye.png')}  />
+        <Image style={styles.iconimg} source={require('../Assets/trash-bin.png')}  />
+        </View>
+      </View>
+    )
 
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.subContainer}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Tutu Todo List</Text>
       </View>
@@ -17,10 +43,13 @@ export default function MainPageContainer() {
           <Text style={styles.mainText}>What's on your bucket list for today?</Text>
           <TextInput style={styles.textInput} placeholder="Write Something.."  onChangeText={setTextInput} value={textInput} />
           <TextInput style={styles.textInput} placeholder="imageUrl" onChangeText={setImageUrl} value={imgUrl} />
-          <View>
-            <Button>Click Me</Button>
-              <Image style={styles.addbutton} source={{uri: 'https://cdn-icons.flaticon.com/png/512/3416/premium/3416075.png?token=exp=1642524540~hmac=63c9843d886a36b8020ab252f89b744f'}} />
+          <View style={styles.addBtnContainer}>
+            <Button title="Add" color="#33B4C6" />
           </View>
+      </View>
+      <ScrollView>
+        <FlatList data={DATA} renderItem={renderItem}  keyExtractor={item => item.id} />
+      </ScrollView>
       </View>
     </SafeAreaView>
   );
@@ -29,8 +58,10 @@ export default function MainPageContainer() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginVertical: 100,
+    backgroundColor: '#EFFDFF',
+},
+subContainer:{
+  marginVertical: 100,
     marginHorizontal: 30
 },
 title:{
@@ -59,5 +90,30 @@ title:{
   addbutton:{
     width: 100,
     height: 100
+  },
+  listItem:{
+    padding: 15,
+    backgroundColor: '#A3F4FF',
+    marginVertical:7,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  iconContainer:{
+    flexDirection: 'row',
+  },
+  listItemText:{
+    fontSize: 15,
+  },
+  addBtn:{
+    marginVertical:20,
+    backgroundColor: '#E4E6E6'
+  },
+  addBtnContainer:{
+    marginVertical: 10
+  },
+  iconimg:{
+    width: 20,
+    height: 20,
+    marginHorizontal: 5,
   }
 });
